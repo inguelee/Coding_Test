e5932ac4147cf90a03c8fe3fb013cd147ac07f01
inguelee <55696282+inguelee@users.noreply.github.com>
2024-04-17 13:20:23 +0900

    1-1상하좌우

---
+++ 이코테(파이썬)/1-1상하좌우.py
@@ -0,0 +1,16 @@
+N = int(input())
+A = list(input().split())
+d = {'L':-1, 'R':1, 'U':-1, 'D':1}
+X, Y = 1, 1
+for i in A:
+    if i == 'L' or i == 'R':
+        Y += d[i]
+    else:
+        X += d[i]
+
+    if X > N or X < 1:
+        X -= d[i]
+    elif Y > N or Y < 1:
+        Y -= d[i]
+
+print(X, Y)
