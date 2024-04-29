d5865a8781c432fa0200b3d909b9494aff8964ef
inguelee <55696282+inguelee@users.noreply.github.com>
2024-04-29 18:56:17 +0900

    1-1상하좌우(2회차)

--- 이코테(파이썬)/4장/1-1상하좌우.py
+++ 이코테(파이썬)/4장/1-1상하좌우.py
@@ -1,16 +1,19 @@
 N = int(input())
 A = input().split()
-d = {'L':-1, 'R':1, 'U':-1, 'D':1}
+
 X, Y = 1, 1
+dataX = {'L':-1, 'R':1}
+dataY = {'U':-1, 'D':1}
+
 for i in A:
-    if i == 'L' or i == 'R':
-        Y += d[i]
+    if i in dataX:
+        X += dataX[i]
     else:
-        X += d[i]
-
+        Y += dataY[i]
+        
     if X > N or X < 1:
-        X -= d[i]
-    elif Y > N or Y < 1:
-        Y -= d[i]
+        X -= dataX[i]
+    if Y > N or Y < 1:
+        Y -= dataY[i]
 
-print(X, Y)
+print(Y, X)
