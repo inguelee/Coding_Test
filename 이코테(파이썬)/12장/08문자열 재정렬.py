b6dadec91d28ded2b6678a93d312c02ce1baccc2
inguelee <4570in@naver.com>
2024-05-16 20:49:34 +0900

    08문자열 재정렬

--- 이코테(파이썬)/12장/08문자열 재정렬.py
+++ 이코테(파이썬)/12장/08문자열 재정렬.py
@@ -1,12 +1,13 @@
 S = list(input())
 
-ss = 0
 S.sort()
+n = 0
 
 for i in range(len(S)):
     try:
-        ss += int(S[i])
+        n += int(S[i])
     except:
-        result = ''.join(S[i:]) + str(ss)
+        result = ''.join(S[i:]) + str(n)
         break
+
 print(result)
