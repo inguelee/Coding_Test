c248f5293947459af66a7b70bd72495411d8824b
inguelee <4570in@naver.com>
2024-05-16 13:10:26 +0900

    41이 될 때까지

--- 이코테(파이썬)/3장/41이 될 때까지.py
+++ 이코테(파이썬)/3장/41이 될 때까지.py
@@ -3,13 +3,11 @@ N, K = map(int, input().split())
 result = 0
 
 while N >= K:
-    c = N % K
-    result += c
-    N -= c
-
-    N //= K
+    result += N % K
+    N -= (N % K)
+    
+    N = N // K
     result += 1
 
 result += (N % K) - 1
-
 print(result)
