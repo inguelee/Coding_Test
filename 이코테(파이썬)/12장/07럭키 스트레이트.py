a432b1c0e4a36741c25a10bccd2797ffbfc59446
inguelee <4570in@naver.com>
2024-05-16 20:41:26 +0900

    07럭키 스트레이트

--- 이코테(파이썬)/12장/07럭키 스트레이트.py
+++ 이코테(파이썬)/12장/07럭키 스트레이트.py
@@ -1,15 +1,8 @@
 N = input()
+a = sum(map(int, N[:len(N)//2]))
+b = sum(map(int, N[len(N)//2:]))
 
-if len(N) % 2:
-    print("READY")
+if a == b:
+    print("LUCKY")
 else:
-    a = N[:len(N)//2]
-    b = N[len(N)//2:]
-
-    a = list(map(int, a))
-    b = list(map(int, b))
-
-    if sum(a) == sum(b):
-        print("LUCKY")
-    else:
-        print("READY")
+    print("READY")
