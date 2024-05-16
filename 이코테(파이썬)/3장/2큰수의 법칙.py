93659c36d8e370951f69f1b7e07cd8b722851bd6
inguelee <4570in@naver.com>
2024-05-16 12:35:15 +0900

    2큰 수의 법칙

--- 이코테(파이썬)/3장/2큰수의 법칙.py
+++ 이코테(파이썬)/3장/2큰수의 법칙.py
@@ -2,8 +2,9 @@ N, M, K = map(int, input().split())
 data = list(map(int, input().split()))
 
 data.sort()
+a = M // (K + 1)
+b = M - a
 
-c = M // (K + 1)
+result = (data[-2] * a) + (data[-1] * b)
 
-result = ((M - c) * data[-1]) + data[-2] * c
 print(result)
