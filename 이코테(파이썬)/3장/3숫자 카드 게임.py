3de245998a7de4bc778866b82eebec49d1baff89
inguelee <4570in@naver.com>
2024-05-17 13:38:51 +0900

    3숫자 카드 게임

--- 이코테(파이썬)/3장/3숫자 카드 게임.py
+++ 이코테(파이썬)/3장/3숫자 카드 게임.py
@@ -1,9 +1,8 @@
 N, M = map(int, input().split())
-
 result = 0
-
 for _ in range(N):
-    value = min(map(int, input().split()))
+    data = list(map(int, input().split()))
+    value = min(data)
     result = max(value, result)
 
 print(result)
