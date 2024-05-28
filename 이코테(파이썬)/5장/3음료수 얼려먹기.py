ec48ba8a8f99f41c9790029ccc93dd09ff64df99
inguelee <4570in@naver.com>
2024-05-28 20:23:40 +0900

    3음료수 얼려 먹기

--- 이코테(파이썬)/5장/3음료수 얼려먹기.py
+++ 이코테(파이썬)/5장/3음료수 얼려먹기.py
@@ -1,30 +1,34 @@
 N, M = map(int, input().split())
 data = []
-visited = [[False] * M for i in range(N)]
 for _ in range(N):
-    data.append(input())
+    data.append(list(map(int, input())))
+
+graph = [[False] * M for _ in range(N)]
+result = 0
+x, y = 0, 0
 
 def dfs(x, y):
     if 0 <= x < N and 0 <= y < M:
-        if visited[x][y]:   # 이미 방문했으면 끝
+        if graph[x][y]:
+            return False
+        
+        if data[x][y]:
             return False
         else:
-            if data[x][y] == '0':
-                visited[x][y] = True
-                dfs(x + 1, y)
-                dfs(x, y + 1)
-                dfs(x - 1, y)
-                dfs(x, y - 1)
-                return True
-            else:
-                return False
+            graph[x][y] = True
+            dfs(x + 1, y)
+            dfs(x, y + 1)
+            dfs(x - 1, y)
+            dfs(x, y - 1)
+            return True
     else:
-        return False                            # 방문하지 않았다면
+        return False
 
-c = 0
+    
+
 for i in range(N):
     for j in range(M):
         if dfs(i, j):
-            c += 1
+            result += 1
 
-print(c)
+print(result)
