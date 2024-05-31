5d9a9688a47ef0409fe644f87c02b9999c19b1e4
inguelee <4570in@naver.com>
2024-05-31 11:00:35 +0900

    4미로 탈출

--- 이코테(파이썬)/5장/4미로 탈출.py
+++ 이코테(파이썬)/5장/4미로 탈출.py
@@ -1,28 +1,27 @@
 from collections import deque
 
 N, M = map(int, input().split())
-data = []
+graph = []
 for _ in range(N):
-    data.append(list(map(int, input())))
+    graph.append(list(map(int, input())))
 
 dx = (-1,0, 1, 0)
 dy = (0, 1, 0,-1)
-#     북,동,남,서
 
 def bfs():
+    x, y = 0, 0
     q = deque()
-    q.append((0, 0))
+    q.append((x, y))
     while q:
         x, y = q.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             if 0 <= nx < N and 0 <= ny < M:
-                if data[nx][ny] == 1: # 한번도 안가본 길
+                if graph[nx][ny] == 1:
+                    graph[nx][ny] = graph[x][y] + 1
                     q.append((nx, ny))
-                    data[nx][ny] = data[x][y] + 1
 
-    return data[N - 1][M - 1]
-            
+bfs()
 
-print(bfs())
+print(graph[N - 1][M - 1])
