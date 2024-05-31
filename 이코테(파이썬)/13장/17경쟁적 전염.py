59271cd28c8decc094a0dae54f761117c9ac7fd2
inguelee <4570in@naver.com>
2024-05-31 16:11:47 +0900

    17경쟁적 전염

--- 이코테(파이썬)/13장/17경쟁적 전염.py
+++ 이코테(파이썬)/13장/17경쟁적 전염.py
@@ -1,31 +1,32 @@
 from collections import deque
 
 N, K = map(int, input().split())
+data = []
 graph = []
-q = []
 for i in range(N):
-    graph.append(list(map(int, input().split())))
+    d = list(map(int, input().split()))
     for j in range(N):
-        if graph[i][j]:
-            q.append((graph[i][j], 0, i, j))
+        if d[j]:
+            graph.append((d[j], 0, i, j))
+    data.append(d)
 S, X, Y = map(int, input().split())
 
-q.sort()
 dx = (-1,0, 1, 0)
 dy = (0, 1, 0,-1)
-q = deque(q)
-
+#     북,동,남,서
+graph.sort()
+q = deque(graph)
 while q:
     v, s, x, y = q.popleft()
+    if s == S:
+        break
+
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
         if 0 <= nx < N and 0 <= ny < N:
-            if graph[nx][ny] == 0:
-                graph[nx][ny] = v
+            if data[nx][ny] == 0:
+                data[nx][ny] = v
                 q.append((v, s + 1, nx, ny))
 
-    if s == (S - 1):
-        break
-
-print(graph[X - 1][Y - 1])
+print(data[X - 1][Y - 1])
