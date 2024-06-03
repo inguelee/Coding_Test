68dc57ca12fa9b78eb13854bca571b38b55ba466
inguelee <4570in@naver.com>
2024-06-03 13:25:08 +0900

    3음료수 얼려 먹기

--- 이코테(파이썬)/5장/3음료수 얼려먹기.py
+++ 이코테(파이썬)/5장/3음료수 얼려먹기.py
@@ -1,22 +1,24 @@
 N, M = map(int, input().split())
-graph = list()
+graph = []
 for _ in range(N):
     graph.append(list(map(int, input())))
 
 result = 0
+dx = (-1,0, 1, 0)
+dy = (0, 1, 0,-1)
+#     북,동,남,서
 
 def dfs(x, y):
     if 0 <= x < N and 0 <= y < M:
         if graph[x][y]:
             return False
-        else:
+        else:    
             graph[x][y] = 1
-            dfs(x + 1, y)
-            dfs(x, y + 1)
-            dfs(x - 1, y)
-            dfs(x, y - 1)
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                dfs(nx, ny)
             return True
-    return False
 
 for i in range(N):
     for j in range(M):
