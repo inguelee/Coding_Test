d87f655069f6cfcfe9dd0bdd1f38e493659b2289
inguelee <55696282+inguelee@users.noreply.github.com>
2024-06-26 21:36:32 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -1,13 +1,12 @@
 from sys import stdin
 
-data = (1,1,2,2,2,8)
+data = [1,1,2,2,2,8]
 
 sub = tuple(map(int, stdin.readline().strip().split()))
 
-result = [0] * 6
 
 for i in range(6):
-    result[i] = data[i] - sub[i]
+    data[i] = data[i] - sub[i]
 
-for i in result:
+for i in data:
     print(i, end=' ')
