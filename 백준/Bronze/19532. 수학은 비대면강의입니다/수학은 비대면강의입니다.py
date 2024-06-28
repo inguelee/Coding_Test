b6327fcda2a33e727bcba70e15109cc3350174a4
inguelee <55696282+inguelee@users.noreply.github.com>
2024-06-28 20:09:52 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -2,7 +2,5 @@ from sys import stdin
 
 a,b,c,d,e,f = map(int, stdin.readline().strip().split())
 
-for i in range(-999, 1000):
-    for j in range(-999, 1000):
-        if (a * i) + (b * j) == c and (d * i) + (e * j) == f:
-            print(i, j)
+
+print((c*e-b*f)//(a*e-b*d), (a*f-d*c)//(a*e-b*d))
